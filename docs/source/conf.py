b412ec28acd0edd65be11aad50fbbd4c07f411c9
MetallerTM <brunofrancesco1998@gmail.com>
2025-10-20 14:45:32 +0200

    Modified files for RTD0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ r"""
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))  
 
 # -- Project information -----------------------------------------------------
 
